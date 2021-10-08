9d9e97d55393c43f3674e658145b92b9787c068e
Duy-Dev1512 <88997688+Duy-Dev1512@users.noreply.github.com>
2021-10-08 11:57:11 +0700

    Create daThuc.c

---
+++ Danh_Sach_Lien_Ket/daThuc.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+
+
+typedef struct {
+    double he_so;
+    int bac;
+} DonThuc;
+
+struct Node{
+    DonThuc e;
+    struct Node* Next;
+};
+typedef struct Node* Position;
+typedef Position DaThuc;
+
+DaThuc khoitao(){
+	DaThuc pD;
+	
+	return pD->Next=NULL; 
+}
+
+
+void chenDonThuc(DonThuc s, DaThuc *pD){
+	
+}
+
+DaThuc nhapDaThuc(){
+	int n;
+	scanf("%d", &n);
+	DaThuc D=khoitao();
+	int i;
+	DonThuc d;
+	for(i=1; i<=n; i++){
+		scanf("%lf%d", &d.he_so, &d.bac);
+		chenDonThuc(d,&D);
+	}
+	return D;
+} 
+
+// DaThuc tinhDaoHam(DaThuc D){
+// 	DaThuc kq=khoitao();
+// 	Position d=D;
+// 	struct Node*  
+// 	while(d->Next!=NULL){
+// 		DonThuc F= 
+// 	} 
+	
+// }
+
+DaThuc congDaThuc(DaThuc D1, DaThuc D2){
+	DaThuc kq=khoitao();
+	Position P=D1;
+	Position Q=D2;
+	while(P->Next!=NULL){
+		chenDonThuc(P->Next->e,&kq);
+		P=P->Next;
+	}
+	while(Q->Next!=NULL){
+		chenDonThuc(Q->Next->e,&kq);
+		Q=Q->Next;
+	}
+	return kq;
+}
+
+DaThuc nhanDaThuc(DaThuc D1,DaThuc D2){
+    DaThuc kq=khoitao();
+    Position P = D1;
+    Position Q ;
+    DonThuc temp ;
+    while(P->Next != NULL){
+    	Q = D2;
+        while(Q->Next != NULL){
+            temp.he_so =P->Next->e.he_so*Q->Next->e.he_so;
+            temp.bac =P->Next->e.bac+Q->Next->e.bac;
+            chenDonThuc(temp,&kq);
+            Q=Q->Next;
+        }
+        P=P->Next;
+    }
+    return kq;
+}
+
+int main(){
+	
+	return 0;
+}
